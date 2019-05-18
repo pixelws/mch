2bc30880423d1ef9f7b46f2ffeefd11ded37d149
CaptnAwesome <witek@PL577164.frontierlocal.net>
2019-05-18 19:06:52 -0400

    logo update

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'MCH Test',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Sample site for MCH',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
